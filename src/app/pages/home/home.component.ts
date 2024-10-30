52327f8cc128c58e9cb3623f7e33d62c62188b17
nogueirawadson <nogueirawadson@outlook.pt>
2024-10-30 20:42:04 -0300

    components

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuTitleComponent } from "../../components/menu-title/menu-title.component";
+import { BigCardComponent } from "../../components/big-card/big-card.component";
+import { SmallCardComponent } from "../../components/small-card/small-card.component";
+import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [MenuTitleComponent, 
+  BigCardComponent,
+  SmallCardComponent,
+  MenuBarComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements OnInit{
+
+  constructor(){
+  }
+  ngOnInit(): void {
+   
+  }
+
+}
